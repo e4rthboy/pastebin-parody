674afc4f0cb31e0421d57afd504f25a0888bfe7f
Nikita Sharipov <nick.sharipov@gmail.com>
2021-10-09 06:46:00 +0300

    init

---
+++ common/enums/SyntaxTypeEnum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace common\enums;
+
+class SyntaxTypeEnum
+{
+    public const SYNTAX_RAW = 1;
+    public const SYNTAX_PHP = 2;
+    public const SYNTAX_CSHARP = 3;
+    public const SYNTAX_JS = 4;
+
+    public static function getNamesList(): array
+    {
+        return [
+            self::SYNTAX_RAW => 'Неопределенный',
+            self::SYNTAX_PHP => 'PHP',
+            self::SYNTAX_CSHARP => 'C#',
+            self::SYNTAX_JS => 'JavaScript',
+        ];
+    }
+}
